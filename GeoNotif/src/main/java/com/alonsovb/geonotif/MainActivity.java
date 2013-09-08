d57a3841f3cd910da2c6e306ec9437308b420602
Alonso Vega <lavb91@gmail.com>
2013-09-07 20:13:40 -0500

    Added base project files

---
+++ GeoNotif/src/main/java/com/alonsovb/geonotif/MainActivity.java
@@ -0,0 +1,25 @@
+package com.alonsovb.geonotif;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    
+}
